e46ca01d7ad01b41c4281557abe750ed7222b903
JarlVPN <Jarlvpn@protonmail.com>
2022-05-24 14:05:55 +0200

    Fix

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import './App.css';
-import './chuj.svg'
-import './Animation.css'
-import './../../index.css'
+import './../../Components/CSS/App.css';
+import './../../Components/CSS/Animation.css'
 import video from "./../../images/video.mp4";
 import { render } from "react-dom"
 
@@ -16,20 +14,21 @@ const Home = () => {
   <video autoPlay loop muted id='video'>
     <source src={video} type='video/mp4'/>
   </video>
-  <header className="Nav">
+  <div>
     <div className='yes text-focus-in'>
     <div className="hero-text">
       <h1><span className="blue">Software </span> House</h1>
         <p>Guarantee of execution of the order within <span className="blue1">30 Days, </span> otherwise money back.</p>
     </div>
     </div>
-      <div className="text2">
-      <a href="roxy.pl"><div className='buttonguton'>
-        <p className='center'>Get Started </p>
-        </div></a>
-        
 
-      </div>
+    <div className="text2">
+      <a href="roxy.pl">
+        <div className='buttonguton'>
+        <p className='center'>Get Started </p>
+        </div>
+      </a>
+    </div>
 
     <div className='pis App text-focus-in'>
       <div className='element2'><h2>#TITLE</h2>
@@ -40,29 +39,24 @@ const Home = () => {
         <div className='features img'>Tworzenie błędów które pozniej naprawiamy</div>
         <div className='features img'>Robienie Exploitów w kodzie</div>
       </div>
-      <div className='logov3'></div>
-    </div>
-
-    <div className='pis App text-focus-in'>
-      <div className='noclass'></div>
-      <div className='wtf'>
-      <div className='element2'><h2>Jebane responsywne strony</h2>
-      <h3 className='text'>Ecommerce</h3>
-      <p className='p'>We build online stores that are limited only by your imagination. We optimize them in terms of marketing, because each store must be able to sell. We use batch methods, and if you use these methods, we write the basics of the modules as needed. 
-      </p>
+      <div>
+        <div className='logov3'></div>
       </div>
     </div>
 
-    <div className='text-focus-in text'>
-
-      
+      <div className='pis App text-focus-in '>
+        <div>
+          <div className='noclass'></div>
+        </div>
+      <div>
+        <div className='element2'><h2>Jebane responsywne strony</h2></div>
+        <h3 className='text'>Ecommerce</h3>
+        <p className='p'>We build online stores that are limited only by your imagination. We optimize them in terms of marketing, because each store must be able to sell. We use batch methods, and if you use these methods, we write the basics of the modules as needed. 
+        </p>
+      </div>
+      </div>
     </div>
-
-      
-    </div>
-
-      </header>
-    </div>
+  </div>
   )
 }
 export default Home;
