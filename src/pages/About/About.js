c24b24cc87829729e625adf6b7a26383e9b94720
JarlVPN <Jarlvpn@protonmail.com>
2022-09-19 01:32:01 +0200

    About us finished for Desktop user's/UHN5Y2hhIHNpYWRhIG9kIHR5Y2ggbGFzZWs=

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -5,80 +5,106 @@ import '../../Components/CSS/About.css'
 
 const About = () => {
   return (
-    <div className="container">
-    <div><h1>title</h1></div>
+    <div className="container tech">
+      <div data-aos="fade-up" data-aos-delay="200">
+        <div className='margin'>
+          <h3 className='collumns'>About Our Team</h3>
+        </div>
+        <div>
+          <p className='margin center'>We have been creating web applications since 1999. The experience gained during this time allows us to respond to the needs of our clients.  </p>
+        </div>
+      </div>
 
     <div className='aboutusclass'>
       <div className='gridv3 position2'>
-        <div className='latdoswiadczenia30'>
-        <div className='workernumber'>1</div>
-        <div className='image-name1'></div>
-         <div><p className='center'>30 lat doświadeczenia ale mnie to wkurwia</p></div>
-          <div className='effect'>
-            <div className='backgroundeffect1'></div>
-            <div className='backgroundeffect2'></div>
-          </div>
+        <div className='latdoswiadczenia30' data-aos="fade-up" data-aos-delay="100">
+        <div className='image-name1'>
         </div>
-        <div className='latdoswiadczenia30'>
-        <div className='workernumber'>4</div>
-        <div className='image-name1'></div>
-          <div><p className='center'>30 lat doświadeczenia ale mnie to wkurwia</p></div>
+         <div><h2 className=''>Adam Miknik</h2><h2 className='lightblue'>Frontend Developer</h2><p>A Front-End Developer is responsible for developing new user-facing features, determining the structure and design of web pages, building reusable codes, optimizing page loading times.</p></div>
           <div className='effect'>
             <div className='backgroundeffect1'></div>
             <div className='backgroundeffect2'></div>
           </div>
+          <div className='socialinks'>
+          <div className='linkedinbutton'></div>
+         </div>
+        </div>
+        <div className='latdoswiadczenia30' data-aos="fade-up" data-aos-delay="200">
+        <div className='image-name1'>
+        </div>
+         <div><h2 className=''>Natalia Miknik</h2><h2 className='lightblue'>Graphic Designer</h2><p>Graphic designers create visual communications such as adverts, branding, publicity materials and magazine layouts. Graphic designers (who may also be known as graphic artists) utilise graphic design skills to create media products such as magazines, labels, advertising and signage</p></div>
+          <div className='effect'>
+            <div className='backgroundeffect1'></div>
+            <div className='backgroundeffect2'></div>
+          </div>
+          <div className='socialinks'>
+          <div className='linkedinbutton'></div>
+         </div>
         </div>
       </div>
       <div className='gridv3'>
-      <div className='latdoswiadczenia30'>
-      <div className='workernumber'>2</div>
-      <div className='image-name1'></div>
-        <div><p className='center'>30 lat doświadeczenia ale mnie to wkurwia</p></div>
+      <div className='latdoswiadczenia30' data-aos="fade-up" data-aos-delay="300">
+        <div className='image-name1'>
+        </div>
+         <div><h2 className=''>Agata Miknik</h2><h2 className='lightblue'>CEO & Sponsor</h2><p>The Chief Executive Officer (CEO) is the highest-ranking executive of any company. They are responsible for ensuring that the business operates at a profit and meets its goals.</p></div>
           <div className='effect'>
             <div className='backgroundeffect1'></div>
             <div className='backgroundeffect2'></div>
           </div>
-      </div>
-      <div className='latdoswiadczenia30'>
-      <div className='workernumber'>5</div>
-      <div className='image-name1'></div>
-        <div><p className='center'>30 lat doświadeczenia ale mnie to wkurwia</p></div>
+          <div className='socialinks'>
+          <div className='linkedinbutton'></div>
+         </div>
+        </div>
+        <div className='latdoswiadczenia30' data-aos="fade-up" data-aos-delay="400">
+        <div className='image-name1'>
+        </div>
+         <div><h2 className=''>Jarl Szakal</h2><h2 className='lightblue'>Developer</h2><p>Developers, also known as software developers or computer programmers, are responsible for developing, coding, installing, and maintaining software systems.</p></div>
           <div className='effect'>
             <div className='backgroundeffect1'></div>
             <div className='backgroundeffect2'></div>
           </div>
-      </div>
-      <div className='latdoswiadczenia30'>
-      <div className='workernumber'>7</div>
-      <div className='image-name1'></div>
-        <div><p className='center'>30 lat doświadeczenia ale mnie to wkurwia</p></div>
+          <div className='socialinks'>
+          <div className='linkedinbutton'></div>
+         </div>
+        </div>
+        <div className='latdoswiadczenia30' data-aos="fade-up" data-aos-delay="500">
+        <div className='image-name1'>
+        </div>
+         <div><h2 className=''>Jarosław Kaczyński</h2><h2 className='lightblue'>Marketing Director</h2><p>A Marketing Director is a professional who is in charge of managing all aspects related to the production and implementation of any given campaign. They're responsible for strategizing and analyzing and leading their team while making sure everything runs smoothly from start to finish with clients or colleagues alike.</p></div>
           <div className='effect'>
             <div className='backgroundeffect1'></div>
             <div className='backgroundeffect2'></div>
           </div>
-      </div>
+          <div className='socialinks'>
+          <div className='linkedinbutton'></div>
+         </div>
+        </div>
       </div>
       <div className='gridv3 position2'>
-      <div className='latdoswiadczenia30'>
-        <div className='workernumber'>3</div>
-        <div className='image-name1'></div>
-        <div>
-          <p className='center'>30 lat doświadeczenia ale mnie to wkurwia</p>
+      <div className='latdoswiadczenia30' data-aos="fade-up" data-aos-delay="600">
+        <div className='image-name1'>
         </div>
+         <div><h2 className=''>Krzysztof Paliga</h2><h2 className='lightblue'>Backend Developer</h2><p>Back-end developers are the experts who build and maintain the mechanisms that process data and perform actions on websites.</p></div>
           <div className='effect'>
             <div className='backgroundeffect1'></div>
             <div className='backgroundeffect2'></div>
           </div>
-      </div>
-      <div className='latdoswiadczenia30'>
-      <div className='workernumber'>5</div>
-      <div className='image-name1'></div>
-          <div><p className='center'>30 lat doświadeczenia ale mnie to wkurwia</p></div>
+          <div className='socialinks'>
+          <div className='linkedinbutton'></div>
+         </div>
+        </div>
+        <div className='latdoswiadczenia30' data-aos="fade-up" data-aos-delay="700">
+        <div className='image-name1'>
+        </div>
+         <div><h2 className=''>Jakub Szymkowiak</h2><h2 className='lightblue'>UI/UX Designer</h2><p>UI UX designers create the user interface for an app, website, or other interactive media. Their work includes collaborating with product managers and engineers to gather requirements from users before designing ideas that can be communicated using storyboards. They also process flows or sitemaps.</p></div>
           <div className='effect'>
             <div className='backgroundeffect1'></div>
             <div className='backgroundeffect2'></div>
           </div>
-      </div>
+          <div className='socialinks'>
+          <div className='linkedinbutton'></div>
+         </div>
+        </div>
       </div>
     </div>
         
