172203d689de68cb4ac00ff37970a4797c6f0a31
JarlVPN <Jarlvpn@protonmail.com>
2022-09-20 02:35:42 +0200

    About us finished for Phones / UHN5Y2hhIGRhbGVqIHNpZWR6aSBsdWR6aWUgd3ltYWdhamEgMzAwIGxhdCBkb8Wbd2lhZGN6ZW5pYSBuYSBqdW5pb3Jh

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -8,7 +8,7 @@ const About = () => {
     <div className="container tech">
       <div data-aos="fade-up" data-aos-delay="200">
         <div className='margin'>
-          <h3 className='collumns'>About Our Team</h3>
+          <h3 className='collumns lightpurple'>About Our Team</h3>
         </div>
         <div>
           <p className='margin center'>We have been creating web applications since 1999. The experience gained during this time allows us to respond to the needs of our clients.  </p>
@@ -18,9 +18,14 @@ const About = () => {
     <div className='aboutusclass'>
       <div className='gridv3 position2'>
         <div className='latdoswiadczenia30' data-aos="fade-up" data-aos-delay="100">
+        <div className='workernumber'><h2 className='numberofperson'>1</h2></div>
         <div className='image-name1'>
         </div>
-         <div><h2 className=''>Adam Miknik</h2><h2 className='lightblue'>Frontend Developer</h2><p>A Front-End Developer is responsible for developing new user-facing features, determining the structure and design of web pages, building reusable codes, optimizing page loading times.</p></div>
+         <div>
+            <h2>Adam Miknik</h2>
+            <h2 className='lightblue'>Frontend Developer</h2>
+            <p>A Front-End Developer is responsible for developing new user-facing features, determining the structure and design of web pages, building reusable codes, optimizing page loading times.</p>
+          </div>
           <div className='effect'>
             <div className='backgroundeffect1'></div>
             <div className='backgroundeffect2'></div>
@@ -30,6 +35,7 @@ const About = () => {
          </div>
         </div>
         <div className='latdoswiadczenia30' data-aos="fade-up" data-aos-delay="200">
+        <div className='workernumber'><h2 className='numberofperson'>4</h2></div>
         <div className='image-name1'>
         </div>
          <div><h2 className=''>Natalia Miknik</h2><h2 className='lightblue'>Graphic Designer</h2><p>Graphic designers create visual communications such as adverts, branding, publicity materials and magazine layouts. Graphic designers (who may also be known as graphic artists) utilise graphic design skills to create media products such as magazines, labels, advertising and signage</p></div>
@@ -44,6 +50,7 @@ const About = () => {
       </div>
       <div className='gridv3'>
       <div className='latdoswiadczenia30' data-aos="fade-up" data-aos-delay="300">
+      <div className='workernumber'><h2 className='numberofperson'>2</h2></div>
         <div className='image-name1'>
         </div>
          <div><h2 className=''>Agata Miknik</h2><h2 className='lightblue'>CEO & Sponsor</h2><p>The Chief Executive Officer (CEO) is the highest-ranking executive of any company. They are responsible for ensuring that the business operates at a profit and meets its goals.</p></div>
@@ -56,6 +63,7 @@ const About = () => {
          </div>
         </div>
         <div className='latdoswiadczenia30' data-aos="fade-up" data-aos-delay="400">
+        <div className='workernumber'><h2 className='numberofperson'>5</h2></div>
         <div className='image-name1'>
         </div>
          <div><h2 className=''>Jarl Szakal</h2><h2 className='lightblue'>Developer</h2><p>Developers, also known as software developers or computer programmers, are responsible for developing, coding, installing, and maintaining software systems.</p></div>
@@ -68,9 +76,10 @@ const About = () => {
          </div>
         </div>
         <div className='latdoswiadczenia30' data-aos="fade-up" data-aos-delay="500">
+        <div className='workernumber'><h2 className='numberofperson'>7</h2></div>
         <div className='image-name1'>
         </div>
-         <div><h2 className=''>Jarosław Kaczyński</h2><h2 className='lightblue'>Marketing Director</h2><p>A Marketing Director is a professional who is in charge of managing all aspects related to the production and implementation of any given campaign. They're responsible for strategizing and analyzing and leading their team while making sure everything runs smoothly from start to finish with clients or colleagues alike.</p></div>
+         <div><h2 className=''>Jarosław Kaczyński</h2><h2 className='lightblue'>Marketing Director</h2><p>A Marketing Director is a professional who is in charge of managing all aspects related to the production and implementation of any given campaign.</p></div>
           <div className='effect'>
             <div className='backgroundeffect1'></div>
             <div className='backgroundeffect2'></div>
@@ -82,6 +91,7 @@ const About = () => {
       </div>
       <div className='gridv3 position2'>
       <div className='latdoswiadczenia30' data-aos="fade-up" data-aos-delay="600">
+      <div className='workernumber'><h2 className='numberofperson'>3</h2></div>
         <div className='image-name1'>
         </div>
          <div><h2 className=''>Krzysztof Paliga</h2><h2 className='lightblue'>Backend Developer</h2><p>Back-end developers are the experts who build and maintain the mechanisms that process data and perform actions on websites.</p></div>
@@ -94,9 +104,10 @@ const About = () => {
          </div>
         </div>
         <div className='latdoswiadczenia30' data-aos="fade-up" data-aos-delay="700">
+        <div className='workernumber'><h2 className='numberofperson'>6</h2></div>
         <div className='image-name1'>
         </div>
-         <div><h2 className=''>Jakub Szymkowiak</h2><h2 className='lightblue'>UI/UX Designer</h2><p>UI UX designers create the user interface for an app, website, or other interactive media. Their work includes collaborating with product managers and engineers to gather requirements from users before designing ideas that can be communicated using storyboards. They also process flows or sitemaps.</p></div>
+         <div><h2 className=''>Jakub Szymkowiak</h2><h2 className='lightblue'>UI/UX Designer</h2><p>Their work includes collaborating with product managers and engineers to gather requirements from users before designing ideas that can be communicated using storyboards. They also process flows.</p></div>
           <div className='effect'>
             <div className='backgroundeffect1'></div>
             <div className='backgroundeffect2'></div>
--- src/pages/Faq/Faq.js
+++ src/pages/Faq/Faq.js
@@ -10,42 +10,165 @@ function Faq() {
   const [thumbsSwiper] = useState(null);
   const [controlledSwiper] = useState(null);
 
-  const gowno = [];
+  const person1 = [];
   for (let i = 0; i < 1; i += 1) {
-    gowno.push(
+    person1.push(
       <SwiperSlide key={`thumb-${i}`} tag="li" style={{ listStyle: 'none' }}>
         <div className='black none'>
-          <div className='fristcard none'>
-          <h1>chuj</h1>
+          <div className='latdoswiadczenia30'>
+          <div className='workernumber'><h2 className='numberofperson'>1</h2></div>
+          <div className='image-name1'>
+        </div>
+         <div>
+            <h2>Adam Miknik</h2>
+            <h2 className='lightblue'>Frontend Developer</h2>
+            <p>A Front-End Developer is responsible for developing new user-facing features, determining the structure and design of web pages, building reusable codes, optimizing page loading times.</p>
+          </div>
+          <div className='effect'>
+            <div className='backgroundeffect1'></div>
+            <div className='backgroundeffect2'></div>
+          </div>
+          <div className='socialinks'>
+          <div className='linkedinbutton'></div>
+         </div>
          </div>
         </div>
       </SwiperSlide>
     );
   } 
-  const dzialaj = [];
+  const person2 = [];
   for (let i = 0; i < 1; i += 1) {
-    dzialaj.push(
+    person2.push(
       <SwiperSlide key={`thumb-${i}`} tag="li" style={{ listStyle: 'none' }}>
          <div className='black'>
-          <div className='fristcard'>
-          <h1>chuj</h1>
+          <div className='latdoswiadczenia30'>
+            <div className='workernumber'><h2 className='numberofperson'>2</h2></div>
+          <div className='image-name1'>
+        </div>
+        <div><h2 className=''>Agata Miknik</h2><h2 className='lightblue'>CEO & Sponsor</h2><p>The Chief Executive Officer (CEO) is the highest-ranking executive of any company. They are responsible for ensuring that the business operates at a profit and meets its goals.</p></div>
+          <div className='effect'>
+            <div className='backgroundeffect1'></div>
+            <div className='backgroundeffect2'></div>
+          </div>
+          <div className='socialinks'>
+          <div className='linkedinbutton'></div>
+         </div>
          </div>
         </div>
       </SwiperSlide>
     );
   }
-  const jebac = [];
+  const person3 = [];
   for (let i = 0; i < 1; i += 1) {
-    jebac.push(
+    person3.push(
       <SwiperSlide key={`thumb-${i}`} tag="li" style={{ listStyle: 'none' }}>
         <div className='black'>
-          <div className='fristcard'>
-          <h1>chuj</h1>
+          <div className='latdoswiadczenia30'>
+          <div className='workernumber'><h2 className='numberofperson'>3</h2></div>
+          <div className='image-name1'>
+        </div>
+        <div><h2 className=''>Krzysztof Paliga</h2><h2 className='lightblue'>Backend Developer</h2><p>Back-end developers are the experts who build and maintain the mechanisms that process data and perform actions on websites.</p></div>
+          <div className='effect'>
+            <div className='backgroundeffect1'></div>
+            <div className='backgroundeffect2'></div>
+          </div>
+          <div className='socialinks'>
+          <div className='linkedinbutton'></div>
+         </div>
          </div>
         </div>
       </SwiperSlide>
     );
   }
+  const person4 = [];
+  for (let i = 0; i < 1; i += 1) {
+    person4.push(
+      <SwiperSlide key={`thumb-${i}`} tag="li" style={{ listStyle: 'none' }}>
+        <div className='black'>
+          <div className='latdoswiadczenia30'>
+          <div className='workernumber'><h2 className='numberofperson'>4</h2></div>
+          <div className='image-name1'>
+        </div>
+        <div><h2 className=''>Natalia Miknik</h2><h2 className='lightblue'>Graphic Designer</h2><p>Graphic designers create visual communications such as adverts, branding, publicity materials and magazine layouts. Graphic designers (who may also be known as graphic artists) utilise graphic design skills to create media products such as magazines, labels, advertising and signage</p></div>
+          <div className='effect'>
+            <div className='backgroundeffect1'></div>
+            <div className='backgroundeffect2'></div>
+          </div>
+          <div className='socialinks'>
+          <div className='linkedinbutton'></div>
+         </div>
+         </div>
+        </div>
+      </SwiperSlide>
+    );
+  }
+  const person5 = [];
+  for (let i = 0; i < 1; i += 1) {
+    person5.push(
+      <SwiperSlide key={`thumb-${i}`} tag="li" style={{ listStyle: 'none' }}>
+        <div className='black'>
+          <div className='latdoswiadczenia30'>
+          <div className='workernumber'><h2 className='numberofperson'>5</h2></div>
+          <div className='image-name1'>
+        </div>
+        <div><h2 className=''>Jarl Szakal</h2><h2 className='lightblue'>Developer</h2><p>Developers, also known as software developers or computer programmers, are responsible for developing, coding, installing, and maintaining software systems.</p></div>
+          <div className='effect'>
+            <div className='backgroundeffect1'></div>
+            <div className='backgroundeffect2'></div>
+          </div>
+          <div className='socialinks'>
+          <div className='linkedinbutton'></div>
+         </div>
+         </div>
+        </div>
+      </SwiperSlide>
+    );
+  }
+  const person6 = [];
+  for (let i = 0; i < 1; i += 1) {
+    person6.push(
+      <SwiperSlide key={`thumb-${i}`} tag="li" style={{ listStyle: 'none' }}>
+        <div className='black'>
+          <div className='latdoswiadczenia30'>
+          <div className='workernumber'><h2 className='numberofperson'>6</h2></div>
+          <div className='image-name1'>
+        </div>
+        <div><h2 className=''>Jakub Szymkowiak</h2><h2 className='lightblue'>UI/UX Designer</h2><p>Their work includes collaborating with product managers and engineers to gather requirements from users before designing ideas that can be communicated using storyboards. They also process flows.</p></div>
+          <div className='effect'>
+            <div className='backgroundeffect1'></div>
+            <div className='backgroundeffect2'></div>
+          </div>
+          <div className='socialinks'>
+          <div className='linkedinbutton'></div>
+         </div>
+         </div>
+        </div>
+      </SwiperSlide>
+    );
+  }
+  const person7 = [];
+  for (let i = 0; i < 1; i += 1) {
+    person7.push(
+      <SwiperSlide key={`thumb-${i}`} tag="li" style={{ listStyle: 'none' }}>
+        <div className='black'>
+          <div className='latdoswiadczenia30'>
+          <div className='workernumber'><h2 className='numberofperson'>7</h2></div>
+          <div className='image-name1'>
+        </div>
+          <div><h2 className=''>Jarosław Kaczyński</h2><h2 className='lightblue'>Marketing Director</h2><p>A Marketing Director is a professional who is in charge of managing all aspects related to the production and implementation of any given campaign.</p></div>
+          <div className='effect'>
+            <div className='backgroundeffect1'></div>
+            <div className='backgroundeffect2'></div>
+          </div>
+          <div className='socialinks'>
+          <div className='linkedinbutton'></div>
+         </div>
+         </div>
+        </div>
+      </SwiperSlide>
+    );
+  }
+
 
 
   return (
@@ -66,9 +189,13 @@ function Faq() {
         }}
         onReachEnd={() => console.log('Swiper end reached')}
       >
-        {gowno}
-        {dzialaj}
-        {jebac}
+        {person1}
+        {person2}
+        {person3}
+        {person4}
+        {person5}
+        {person6}
+        {person7}
       </Swiper>
     </React.Fragment>
   );
